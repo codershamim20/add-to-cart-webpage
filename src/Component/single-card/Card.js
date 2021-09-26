b9cd32c63654f8a91b564f8a57a3074f9d2a6caf
codershamim20 <shamim.phys@gmail.com>
2021-09-26 21:05:46 +0600

    Modified

--- src/Component/single-card/Card.js
+++ src/Component/single-card/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Card = (props) => {
-    const{name,img,country,designation,salary}=props.card||{}
+    const{name,img,country,rating,designation,salary}=props.card||{}
     // console.log(props.card)
     return (
         <div className="col-md-4">
@@ -15,7 +15,8 @@ const Card = (props) => {
         <h5 className="card-title">Name: {name}</h5>
         <p className="card-text">Role: {designation}</p>
         <p className="card-text">{country} </p>
-        <p className="card-text">Salary: {salary}</p>
+        <p className="card-text">Rating: {rating} </p>
+        <p className="card-text">Salary: ${salary}</p>
         <button type="button" 
         onClick={()=>props.handleAddIteam(props.card)}
         className="btn btn-primary"> <i className="fas fa-shopping-cart"></i> Click To Add</button>
